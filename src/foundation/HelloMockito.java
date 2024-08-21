93000aa56ab39728a0cc0cb00976097a4c4f10ce
wyifan <wyifan@vip.qq.com>
2024-08-22 00:40:49 +0800

    add HelloMockito file

---
+++ src/foundation/HelloMockito.java
@@ -0,0 +1,9 @@
+package foundation;
+
+public class HelloMockito {
+    private String greeting = "Hello, %s, from Mockito!";
+
+    public String greet(String name) {
+        return String.format(greeting, name);
+    }
+}
