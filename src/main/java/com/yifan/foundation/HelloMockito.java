539fdbac84f5bfcaac5401371bc5fc0827875c0f
shawn <wyifan@vip.qq.com>
2024-08-22 09:43:49 +0800

    change the project to using maven

--- src/main/java/com/yifan/foundation/HelloMockito.java
+++ src/main/java/com/yifan/foundation/HelloMockito.java
@@ -1,7 +1,7 @@
-package foundation;
+package com.yifan.foundation;
 
 public class HelloMockito {
-    private String greeting = "Hello, %s, from Mockito!";
+    private String greeting = "Hello %s, from Mockito!";
 
     public String greet(String name) {
         return String.format(greeting, name);
---
+++ src/test/java/com/yifan/foundation/HelloMockitoTest.java
@@ -0,0 +1,21 @@
+package com.yifan.foundation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * package_name: com.yifan.foundation
+ * author: wyifa
+ * date: 2024/8/22
+ * description:
+ */
+class HelloMockitoTest {
+    private HelloMockito helloMockito = new HelloMockito();
+
+    @Test
+    void greetPerson() {
+        String greeting = helloMockito.greet("World");
+        assertEquals("Hello World, from mockito!", greeting);
+    }
+}
