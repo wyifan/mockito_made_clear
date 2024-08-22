55cc5a37307329a2a95a695002dff154a2722b2b
shawn <wyifan@vip.qq.com>
2024-08-22 22:49:43 +0800

    add method to test in astronaut service and and a test to treat when invoke a method that fails.

--- src/test/java/com/yifan/foundation/astro/AstroServiceTest.java
+++ src/test/java/com/yifan/foundation/astro/AstroServiceTest.java
@@ -1,16 +1,24 @@
 package com.yifan.foundation.astro;
 
+import com.yifan.foundation.base.Assignment;
 import com.yifan.foundation.base.AstroResponse;
 import com.yifan.foundation.base.Gateway;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * package_name: com.yifan.foundation.astro
@@ -18,7 +26,19 @@ import static org.junit.jupiter.api.Assertions.assertAll;
  * date: 2024/8/22
  * description:
  */
+@ExtendWith(MockitoExtension.class)
 class AstroServiceTest {
+
+    private final AstroResponse mockAstroResponse =
+            new AstroResponse(7, "Success", Arrays.asList(
+                    new Assignment("John Sheridan", "Babylon 5"),
+                    new Assignment("Susan Ivanova", "Babylon 5"),
+                    new Assignment("Beckett Mariner", "USS Cerritos"),
+                    new Assignment("Brad Boimler", "USS Cerritos"),
+                    new Assignment("Sam Rutherford", "USS Cerritos"),
+                    new Assignment("D'Vana Tendi", "USS Cerritos"),
+                    new Assignment("Ellen Ripley", "Nostromo")
+            ));
 
     @Mock
     private Gateway<AstroResponse> gateway;
@@ -63,5 +83,51 @@ class AstroServiceTest {
                     () -> assertThat(craft).isNotBlank()
             );
         });
+    }
+
+    @Test
+    @DisplayName("Test using mock")
+    void testAstroData_usingInjectedMockGateway() {
+        // mock Gateway created and injected into AstroService using
+        // @Mock and @InjectMock annotations
+        //
+        // set the expectation on the mock
+        when(gateway.getResponse()).thenReturn(mockAstroResponse);
+
+        // Call the method under test
+        Map<String, Long> astroData = service.getAstroData();
+
+        // check the results from the method under test
+        assertThat(astroData)
+                .containsEntry("Babylon 5", 2L)
+                .containsEntry("Nostromo", 1L)
+                .containsEntry("USS Cerritos", 4L);
+
+        astroData.forEach((craft, number) -> {
+            System.out.println(number + " astronauts aboard " + craft);
+
+            assertAll(
+                    () -> assertThat((number)).isPositive(),
+                    () -> assertThat(craft).isNotBlank()
+            );
+        });
+
+        // Verify the stubbed method was called
+        verify(gateway).getResponse();
+    }
+
+    @Test
+    @DisplayName("Test a failure")
+    void testAstroData_usingFailureGateway() {
+        when(gateway.getResponse()).thenThrow(
+                new RuntimeException(new IOException("Network problems"))
+        );
+
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> service.getAstroData())
+                .withCauseInstanceOf(IOException.class)
+                .withMessageContaining("Network problems");
+
+        verify(gateway).getResponse();
     }
 }
