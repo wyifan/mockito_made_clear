8bf98fb8035c1d30e8f695354112f69905c81684
shawn <wyifan@vip.qq.com>
2024-08-22 18:01:12 +0800

    add a inmemoryPersonRepository methods implements.

---
+++ src/main/java/com/yifan/foundation/repository/InMemoryPersonRepository.java
@@ -0,0 +1,48 @@
+package com.yifan.foundation.repository;
+
+import com.yifan.foundation.base.Person;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * package_name: com.yifan.foundation.repository
+ * author: wyifa
+ * date: 2024/8/22
+ * description:
+ */
+public class InMemoryPersonRepository implements PersonRepository {
+    private final List<Person> people = Collections.synchronizedList(new ArrayList<>());
+
+    @Override
+    public Person save(Person person) {
+        synchronized (people) {
+            people.add(person);
+        }
+        return person;
+    }
+
+    @Override
+    public Optional<Person> findById(int id) {
+        Map<Integer, Person> peopleMap = people.stream()
+                .collect(Collectors.toMap(Person::getId, p -> p));
+        return Optional.ofNullable(peopleMap.get(id));
+    }
+
+    @Override
+    public List<Person> findAll() {
+        return people;
+    }
+
+    @Override
+    public long count() {
+        return people.size();
+    }
+
+    @Override
+    public void delete(Person person) {
+        synchronized (people) {
+            people.remove(person);
+        }
+    }
+}
