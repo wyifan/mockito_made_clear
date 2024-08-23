91db57e320c960d9ec48d1e671fb79d5c44a363f
shawn <wyifan@vip.qq.com>
2024-08-24 00:13:58 +0800

    add a translationService implementation.

---
+++ src/main/java/com/yifan/foundation/service/DefaultTranslationService.java
@@ -0,0 +1,15 @@
+package com.yifan.foundation.service;
+
+/**
+ * package_name: com.yifan.foundation.service
+ * author: wyifa
+ * date: 2024/8/24
+ * description:
+ */
+public class DefaultTranslationService implements TranslationService {
+
+    @Override
+    public String translate(String text, String sourceLan, String targetLan) {
+        return text;
+    }
+}
